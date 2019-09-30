ae60ea52d7fbb6b51ee573ade456cf5d1be9801b
Redsun <55924922+redsunmost@users.noreply.github.com>
2019-10-01 00:43:38 +0800

    Use dark theme

---
+++ src/gatsby-theme-material-ui-top-layout/theme.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+  typography: {
+    button: {
+      textTransform: 'none',
+    },
+  },
+})
+
+export default theme
